f25dc3db68d03781f2be98719f5d1d017917e65b
xnscdev <xnscdev@gmail.com>
2021-10-20 17:15:34 -0700

    Load TSS on boot

--- arch/x86_64/gdt.c
+++ arch/x86_64/gdt.c
@@ -14,14 +14,42 @@
    You should have received a copy of the GNU General Public License
    along with PML. If not, see <https://www.gnu.org/licenses/>. */
 
+/*! @file */
+
 #include <pml/gdt.h>
 
-static uint64_t gdt_table[5];
+/*! 
+ * The kernel task state segment, used for interrupts between privilege
+ * levels.
+ */
+
+struct tss kernel_tss;
+
+/*! Stack for interrupts to ring 0 from ring 3, used in TSS */
+static unsigned char int_stack[16384];
+
+/*! The kernel GDT */
+static uint64_t gdt_table[7];
+
+/*! The kernel GDT pointer */
 static struct gdt_ptr gdt_ptr;
+
+/*!
+ * Creates an entry in the global descriptor table.
+ *
+ * @param base the base address of the segment
+ * @param limit the size of the segment
+ * @param rw whether the segment allows write access
+ * @param dc whether to set the direction or conforming bit
+ * @param can_exec whether the segment allows executing code
+ * @param tss whether the segment is a TSS
+ * @param privilege the minimum privilege level required
+ * @return a 64-bit integer suitable as a value in the GDT
+ */
 
 static uint64_t
 gdt_entry (uint32_t base, uint32_t limit, int rw, int dc, int can_exec,
-	   int system_seg, unsigned int privilege)
+	   int tss, unsigned int privilege)
 {
   unsigned char access = GDT_ACC_PRESENT | GDT_ACC_PRIVILEGE (privilege);
   unsigned char flags;
@@ -29,12 +57,15 @@ gdt_entry (uint32_t base, uint32_t limit, int rw, int dc, int can_exec,
     access |= GDT_ACC_RW;
   if (dc)
     access |= GDT_ACC_DC;
-  if (!system_seg)
+  if (tss)
+    access |= GDT_ACC_ACCESS;
+  else
     access |= GDT_ACC_TYPE;
   if (can_exec)
     {
       access |= GDT_ACC_EXECUTE;
-      flags = GDT_FLAG_LONG_CODE;
+      if (!tss)
+	flags = GDT_FLAG_LONG_CODE;
     }
   else
     flags = GDT_FLAG_SIZE;
@@ -46,15 +77,27 @@ gdt_entry (uint32_t base, uint32_t limit, int rw, int dc, int can_exec,
     | ((uint64_t) (base >> 24)      << 56);
 }
 
+/*
+ * Initializes the kernel global descriptor table.
+ */
+
 void
 init_gdt (void)
 {
+  uintptr_t tss = (uintptr_t) &kernel_tss;
+  kernel_tss.rsp0 = (uint64_t) int_stack;
+
   gdt_table[0] = gdt_entry (0, 0, 0, 0, 0, 0, 0);
   gdt_table[1] = gdt_entry (0, 0xffffffff, 1, 0, 1, 0, 0);
   gdt_table[2] = gdt_entry (0, 0xffffffff, 1, 0, 0, 0, 0);
   gdt_table[3] = gdt_entry (0, 0xffffffff, 1, 0, 1, 0, 3);
   gdt_table[4] = gdt_entry (0, 0xffffffff, 1, 0, 0, 0, 3);
+  gdt_table[5] = gdt_entry (tss & 0xffffffff, sizeof (struct tss),
+			    0, 0, 1, 1, 0);
+  gdt_table[6] = tss >> 32;
+
   gdt_ptr.size = sizeof (gdt_table) - 1;
   gdt_ptr.addr = gdt_table;
   load_gdt (&gdt_ptr);
+  load_tss (0x28);
 }
--- include/pml/gdt.h
+++ include/pml/gdt.h
@@ -17,6 +17,11 @@
 #ifndef __PML_GDT_H
 #define __PML_GDT_H
 
+/*!
+ * @file
+ * @brief x86-64 global descriptor tables
+ */
+
 #include <pml/cdefs.h>
 
 #define GDT_ACC_ACCESS         (1 << 0)
@@ -31,13 +36,56 @@
 #define GDT_FLAG_SIZE          (1 << 2)
 #define GDT_FLAG_GRANULARITY   (1 << 3)
 
+/*! Type used as a segment to index into the GDT. */
+typedef unsigned short segment_t;
+
+/*!
+ * Format of a GDT pointer.
+ */
+
 struct gdt_ptr
 {
-  uint16_t size;
-  uint64_t *addr;
+  uint16_t size;                /*!< Number of bytes in GDT minus one */
+  uint64_t *addr;               /*!< Base virtual address of GDT */
 } __packed;
 
+/*!
+ * Format of the long mode task state segment (TSS).
+ */
+
+struct tss
+{
+  uint32_t reserved;
+  uint64_t rsp0;
+  uint64_t rsp1;
+  uint64_t rsp2;
+  uint64_t reserved2;
+  uint64_t ist1;
+  uint64_t ist2;
+  uint64_t ist3;
+  uint64_t ist4;
+  uint64_t ist5;
+  uint64_t ist6;
+  uint64_t ist7;
+  uint16_t reserved3[5];
+  uint16_t iopb;
+} __packed;
+
+/*!
+ * Loads a task state segment.
+ *
+ * @param desc the descriptor of the TSS in the GDT
+ */
+
+static inline void
+load_tss (segment_t desc)
+{
+  __asm__ volatile ("ltr %0" :: "r" (desc));
+}
+
 __BEGIN_DECLS
+
+extern struct tss kernel_tss;
 
 void init_gdt (void);
 void load_gdt (const struct gdt_ptr *ptr);
