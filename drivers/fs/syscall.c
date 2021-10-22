3bb59d9c3308695a9c74e3710a94efbd2ccf57b9
xnscdev <xnscdev@gmail.com>
2021-10-21 20:35:04 -0700

    Added read(2) and write(2) syscalls

---
+++ drivers/fs/syscall.c
@@ -0,0 +1,68 @@
+/* syscall.c -- This file is part of PML.
+   Copyright (C) 2021 XNSC
+
+   PML is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   PML is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with PML. If not, see <https://www.gnu.org/licenses/>. */
+
+/*! @file */
+
+#include <pml/process.h>
+#include <pml/syscall.h>
+#include <errno.h>
+
+/*!
+ * Obtains the file structure from a file descriptor in the current process.
+ * If an error occurs, @ref errno will be set to @ref EBADF.
+ *
+ * @param fd the file descriptor
+ * @return the file structure, or NULL if the file descriptor does not
+ * reference an open file
+ */
+
+static struct fd *
+file_fd (int fd)
+{
+  struct fd_table *fds = &THIS_PROCESS->fds;
+  if (fd < 0 || (size_t) fd >= fds->size)
+    RETV_ERROR (EBADF, NULL);
+  return fds->table[fd];
+  
+}
+
+ssize_t
+sys_read (int fd, void *buffer, size_t len)
+{
+  struct fd *file = file_fd (fd);
+  ssize_t ret;
+  if (!file)
+    return -1;
+  ret = vfs_read (file->vnode, buffer, len, file->offset);
+  if (ret == -1)
+    return -1;
+  file->offset += ret;
+  return ret;
+}
+
+ssize_t
+sys_write (int fd, const void *buffer, size_t len)
+{
+  struct fd *file = file_fd (fd);
+  ssize_t ret;
+  if (!file)
+    return -1;
+  ret = vfs_write (file->vnode, buffer, len, file->offset);
+  if (ret == -1)
+    return -1;
+  file->offset += ret;
+  return ret;
+}
