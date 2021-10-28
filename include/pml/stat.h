ae18ed1723a52bfac2f73010b339a9dd5942891e
xnscdev <xnscdev@gmail.com>
2021-10-27 19:23:23 -0700

    Added backwards-compatible macros for struct stat timestamps

--- include/pml/stat.h
+++ include/pml/stat.h
@@ -81,4 +81,8 @@ struct stat
   blksize_t st_blksize;         /*!< Optimal I/O block size */
 };
 
+#define st_atime                st_atim.tv_sec
+#define st_ctime                st_ctim.tv_sec
+#define st_mtime                st_mtim.tv_sec
+
 #endif
