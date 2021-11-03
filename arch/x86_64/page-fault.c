cbeedef4993acf6d50ca01d379d12f9264dd9f6a
xnscdev <xnscdev@gmail.com>
2021-11-02 22:38:06 -0700

    Implemented copy-on-write in page fault handler

--- arch/x86_64/page-fault.c
+++ arch/x86_64/page-fault.c
@@ -16,9 +16,12 @@
 
 /*! @file */
 
+#include <pml/alloc.h>
 #include <pml/interrupt.h>
+#include <pml/memory.h>
 #include <pml/panic.h>
 #include <pml/process.h>
+#include <string.h>
 
 /*!
  * Handles a page fault. This function will perform necessary copying-on-writes
@@ -32,11 +35,108 @@ void
 int_page_fault (unsigned long err)
 {
   uintptr_t addr;
+  unsigned int pml4e;
+  unsigned int pdpe;
+  unsigned int pde;
+  unsigned int pte;
+  uintptr_t *pml4t;
+  uintptr_t *pdpt;
+  uintptr_t *pdt;
+  uintptr_t *pt;
+  size_t i;
   __asm__ volatile ("mov %%cr2, %0" : "=r" (addr));
 
   /* Assume page faults on the kernel thread are fatal */
   if (!THIS_PROCESS->pid)
     goto fatal;
+
+  /* Check if the error was caused by an attempted write to a read-only
+     page that needs to be copied-on-write */
+  if (err == 7)
+    {
+      pml4t = THIS_THREAD->args.pml4t;
+      pml4e = PML4T_INDEX (addr);
+      if (addr >> 48 != !!(pml4e & 0x100) * 0xffff) /* Check sign extension */
+	goto fatal;
+      if (!(pml4t[pml4e] & PAGE_FLAG_PRESENT))
+	goto fatal;
+      if (pml4t[pml4e] & PAGE_FLAG_COW)
+	{
+	  uintptr_t page = alloc_page ();
+	  uintptr_t *np = (uintptr_t *) PHYS_REL (page);
+	  uintptr_t *cp =
+	    (uintptr_t *) PHYS_REL (ALIGN_DOWN (pml4t[pml4e], PAGE_SIZE));
+	  if (UNLIKELY (!page))
+	    goto fatal;
+	  for (i = 0; i < PAGE_STRUCT_ENTRIES; i++)
+	    {
+	      np[i] = cp[i] | PAGE_FLAG_COW;
+	      np[i] &= ~PAGE_FLAG_RW;
+	    }
+	  pml4t[pml4e] = page | PAGE_FLAG_RW | (pml4t[pml4e] & (PAGE_SIZE - 1));
+	  pml4t[pml4e] &= ~PAGE_FLAG_COW;
+	}
+
+      pdpt = (uintptr_t *) PHYS_REL (ALIGN_DOWN (pml4t[pml4e], PAGE_SIZE));
+      pdpe = PDPT_INDEX (addr);
+      if (!(pdpt[pdpe] & PAGE_FLAG_PRESENT))
+	goto fatal;
+      if (pdpt[pdpe] & PAGE_FLAG_COW)
+	{
+	  uintptr_t page = alloc_page ();
+	  uintptr_t *np = (uintptr_t *) PHYS_REL (page);
+	  uintptr_t *cp =
+	    (uintptr_t *) PHYS_REL (ALIGN_DOWN (pdpt[pdpe], PAGE_SIZE));
+	  if (UNLIKELY (!page))
+	    goto fatal;
+	  for (i = 0; i < PAGE_STRUCT_ENTRIES; i++)
+	    {
+	      np[i] = cp[i] | PAGE_FLAG_COW;
+	      np[i] &= ~PAGE_FLAG_RW;
+	    }
+	  pdpt[pdpe] = page | PAGE_FLAG_RW | (pdpt[pdpe] & (PAGE_SIZE - 1));
+	  pdpt[pdpe] &= ~PAGE_FLAG_COW;
+	}
+
+      pdt = (uintptr_t *) PHYS_REL (ALIGN_DOWN (pdpt[pdpe], PAGE_SIZE));
+      pde = PDT_INDEX (addr);
+      if (!(pdt[pde] & PAGE_FLAG_PRESENT))
+	goto fatal;
+      if (pdt[pde] & PAGE_FLAG_COW)
+	{
+	  uintptr_t page = alloc_page ();
+	  uintptr_t *np = (uintptr_t *) PHYS_REL (page);
+	  uintptr_t *cp =
+	    (uintptr_t *) PHYS_REL (ALIGN_DOWN (pdt[pde], PAGE_SIZE));
+	  if (UNLIKELY (!page))
+	    goto fatal;
+	  for (i = 0; i < PAGE_STRUCT_ENTRIES; i++)
+	    {
+	      np[i] = cp[i] | PAGE_FLAG_COW;
+	      np[i] &= ~PAGE_FLAG_RW;
+	    }
+	  pdt[pde] = page | PAGE_FLAG_RW | (pdt[pde] & (PAGE_SIZE - 1));
+	  pdt[pde] &= ~PAGE_FLAG_COW;
+	}
+
+      pt = (uintptr_t *) PHYS_REL (ALIGN_DOWN (pdt[pde], PAGE_SIZE));
+      pte = PT_INDEX (addr);
+      if (!(pt[pte] & PAGE_FLAG_PRESENT))
+	goto fatal;
+      if (pt[pte] & PAGE_FLAG_COW)
+	{
+	  uintptr_t page = alloc_page ();
+	  if (UNLIKELY (!page))
+	    goto fatal;
+	  memcpy ((void *) PHYS_REL (page),
+		  (void *) PHYS_REL (ALIGN_DOWN (pt[pte], PAGE_SIZE)),
+		  PAGE_SIZE);
+	  pt[pte] = page | PAGE_FLAG_RW | (pt[pte] & (PAGE_SIZE - 1));
+	  pt[pte] &= ~PAGE_FLAG_COW;
+	  vm_clear_page ((void *) ALIGN_DOWN (addr, PAGE_SIZE));
+	  return;
+	}
+    }
 
  fatal:
   panic ("CPU exception: page fault\nVirtual address: %p\n", addr);
